ce4e50c226c8d096ac597b6e87ebc25bef84e90c
IldarYakup <IldarYakup@gmail.com>
2021-09-14 21:07:41 +0300

    Lesson5 complete

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from mainapp.models import Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+#from shop.mainapp.models import ProductCategory
+
+
 def main(request):
     title = 'Главная'
     products = Product.objects.all()
     content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', content)
 
+
 def products(request):
-    return render(request, 'mainapp/products.html')
+    products = Product.objects.all()
+    content = {'products': products}
+    return render(request, 'mainapp/products_list.html', content)
+
+def product(request, pk):
+    product = Product.objects.filter(pk=pk)
+    content = {'products': product}
+    return render(request, 'mainapp/product_page.html', content)
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
 
-def temp1(request):
-    return render(request, 'mainapp/temp1.html')
